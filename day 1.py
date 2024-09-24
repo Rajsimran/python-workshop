60a15dd4febfe0f4b1fdc330dfb41f5f154b6293
Rajsimran <simranraj725060@gmail.com>
2024-09-24 20:00:51 +0530

    Add files via upload

---
+++ day 1.py
@@ -0,0 +1,334 @@
+Python 3.12.6 (tags/v3.12.6:a4a2d2b, Sep  6 2024, 20:11:23) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+import numpy
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    import numpy
+ModuleNotFoundError: No module named 'numpy'
+print("hello world")
+hello world
+2+3
+5
+2**3
+8
+"m
+SyntaxError: unterminated string literal (detected at line 1)
+MCE"*3
+SyntaxError: unterminated string literal (detected at line 1)
+"MEC"*3
+'MECMECMEC'
+"NEC"*3
+'NECNECNEC'
+x=5
+type(5)
+<class 'int'>
+x="Simran"type(x)
+SyntaxError: invalid syntax
+x="Simran"
+type(x)
+<class 'str'>
+
+======================================================== RESTART: C:/Users/simra/AppData/Local/Programs/Python/Python312/file.py =======================================================
+Hello
+world
+
+======================================================== RESTART: C:/Users/simra/AppData/Local/Programs/Python/Python312/file.py =======================================================
+Traceback (most recent call last):
+  File "C:/Users/simra/AppData/Local/Programs/Python/Python312/file.py", line 1, in <module>
+    prin("Hello")
+NameError: name 'prin' is not defined. Did you mean: 'print'?
+
+======================================================== RESTART: C:/Users/simra/AppData/Local/Programs/Python/Python312/file.py =======================================================
+Hello
+Traceback (most recent call last):
+  File "C:/Users/simra/AppData/Local/Programs/Python/Python312/file.py", line 2, in <module>
+    prin("world")
+NameError: name 'prin' is not defined. Did you mean: 'print'?
+
+======================================================== RESTART: C:/Users/simra/AppData/Local/Programs/Python/Python312/file.py =======================================================
+Hello
+world
+x=5
+name="Simran"
+first name="Simran"
+SyntaxError: invalid syntax
+first_name
+Traceback (most recent call last):
+  File "<pyshell#16>", line 1, in <module>
+    first_name
+NameError: name 'first_name' is not defined
+first_name="Simran"
+first_name
+'Simran'
+print("hello world")
+hello world
+x=7
+print(x)
+7
+x=input("Enter a number:")
+Enter a number:6
+print(x)
+6
+x,y=input("Enter two number:").sep()
+Enter two number:4
+Traceback (most recent call last):
+  File "<pyshell#24>", line 1, in <module>
+    x,y=input("Enter two number:").sep()
+AttributeError: 'str' object has no attribute 'sep'
+x=input("Enter a number")
+Enter a number5
+type(x)
+<class 'str'>
+x=int(int("Enter a number"))
+Traceback (most recent call last):
+  File "<pyshell#27>", line 1, in <module>
+    x=int(int("Enter a number"))
+ValueError: invalid literal for int() with base 10: 'Enter a number'
+x=int(input("Enter a number:"))
+Enter a number:7
+type=(7)
+
+type(7)
+Traceback (most recent call last):
+  File "<pyshell#31>", line 1, in <module>
+    type(7)
+TypeError: 'int' object is not callable
+x=int(input("Enter a number:"))
+Enter a number:7
+type(7)
+Traceback (most recent call last):
+  File "<pyshell#38>", line 1, in <module>
+    type(7)
+TypeError: 'int' object is not callable
+x=int(input("Enter a number"))
+Enter a number7
+type(7)
+Traceback (most recent call last):
+  File "<pyshell#40>", line 1, in <module>
+    type(7)
+TypeError: 'int' object is not callable
+x=int(input("Enter a number:")
+      type(7)
+      
+SyntaxError: '(' was never closed
+x=int(input("Enter a number:"))
+      
+Enter a number:7
+type(7)
+      
+Traceback (most recent call last):
+  File "<pyshell#44>", line 1, in <module>
+    type(7)
+TypeError: 'int' object is not callable
+x=int(input("Enter a number:"))
+      
+Enter a number:7
+type(7)
+      
+Traceback (most recent call last):
+  File "<pyshell#46>", line 1, in <module>
+    type(7)
+TypeError: 'int' object is not callable
+x=int(input("Enter a number:"))
+      
+Enter a number:7
+type(7)
+      
+Traceback (most recent call last):
+  File "<pyshell#48>", line 1, in <module>
+    type(7)
+TypeError: 'int' object is not callable
+x=int(input("Enter a number:"))
+      
+Enter a number:7
+type(7)
+      
+Traceback (most recent call last):
+  File "<pyshell#50>", line 1, in <module>
+    type(7)
+TypeError: 'int' object is not callable
+x=int(input("Enter a number:"))
+      
+Enter a number: 7
+type(7)
+      
+Traceback (most recent call last):
+  File "<pyshell#52>", line 1, in <module>
+    type(7)
+TypeError: 'int' object is not callable
+x,y=input("Enter atwo numbers :").split()
+      
+Enter atwo numbers :6 7
+x=int(X)
+      
+Traceback (most recent call last):
+  File "<pyshell#54>", line 1, in <module>
+    x=int(X)
+NameError: name 'X' is not defined. Did you mean: 'x'?
+x,y
+      
+('6', '7')
+=
+x,y=input("Enter two numbers:").split()
+      
+Enter two numbers:6 7
+x+y
+      
+'67'
+print("Hello \n world")
+      
+Hello 
+ world
+
+======================================================== RESTART: C:/Users/simra/AppData/Local/Programs/Python/Python312/file.py =======================================================
+Hello
+world
+Simran#Anjali#Rani are friends
+2**3
+      
+8
+4//2
+      
+2
+4.5//2
+      
+2.0
+5//2
+      
+2
+x=5
+      
+x**=2
+      
+x
+      
+25
+x=["Rose",Lotus"]
+   
+SyntaxError: unterminated string literal (detected at line 1)
+x=["Rose","Lotus"]
+   
+print("Rose" in x)_
+   
+SyntaxError: invalid syntax
+print("Rose" in x)
+   
+True
+print("Riya" in x)
+   
+False
+print("Riya" not in x)
+   
+True
+print("Rose
+      
+SyntaxError: unterminated string literal (detected at line 1)
+"print("Rose" not in x)
+      
+SyntaxError: unterminated string literal (detected at line 1)
+print("Rose" not in x)
+      
+False
+x=6
+      
+y=7
+      
+print(x is y)
+      
+False
+y=x
+      
+print(x is y)
+      
+True
+x="Simran"
+      
+y=7
+      
+print(x is y)
+      
+False
+z="7"print(y is z)
+      
+SyntaxError: invalid syntax
+z="7"
+      
+print(y is z)
+      
+False
+#This is comment
+      
+#This is second comment
+      
+#This is third comment
+      
+egwuiehioeoiuerhfuerhfciueruh
+      
+Traceback (most recent call last):
+  File "<pyshell#89>", line 1, in <module>
+    egwuiehioeoiuerhfuerhfciueruh
+NameError: name 'egwuiehioeoiuerhfuerhfciueruh' is not defined
+"rferukrvhrkuhruvrhu
+      
+SyntaxError: unterminated string literal (detected at line 1)
+""'
+      
+SyntaxError: unterminated string literal (detected at line 1)
+"bhfdfvyhrgjyugjdfjh
+      
+SyntaxError: unterminated string literal (detected at line 1)
+"""vdggfydgfygyd
+hjbdfjhfhdkfuu
+geygfgfiuhfher
+""
+"""
+      
+'vdggfydgfygyd\nhjbdfjhfhdkfuu\ngeygfgfiuhfher\n""\n'
+KeyboardInterrupt
+
+======================================================== RESTART: C:/Users/simra/AppData/Local/Programs/Python/Python312/file.py =======================================================
+Hello
+world
+Simran#Anjali#Rani are friends
+>>> KeyboardInterrupt
+>>> 
+======================================================== RESTART: C:/Users/simra/AppData/Local/Programs/Python/Python312/file.py =======================================================
+Hello
+world
+Simran#Anjali#Rani are friends
+>>> 
+======================================================== RESTART: C:/Users/simra/AppData/Local/Programs/Python/Python312/file.py =======================================================
+>>> 
+===================================================================== RESTART: C:/Users/Public/python/.git/file.py =====================================================================
+>>> 
+>>> if(x=5) :
+...       
+SyntaxError: invalid syntax. Maybe you meant '==' or ':=' instead of '='?
+>>> if (x==5);
+...       
+SyntaxError: invalid syntax
+>>> if(x==5);
+...       
+SyntaxError: invalid syntax
+>>> if(x==5):
+...       print("Hello")
+... 
+...       
+Traceback (most recent call last):
+  File "<pyshell#104>", line 1, in <module>
+    if(x==5):
+NameError: name 'x' is not defined
+>>> 
+===================================================================== RESTART: C:/Users/Public/python/.git/file.py =====================================================================
+Hello
+Gec Vaishali
+>>> 
+===================================================================== RESTART: C:/Users/Public/python/.git/file.py =====================================================================
+>>> 
+===================================================================== RESTART: C:/Users/Public/python/.git/file.py =====================================================================
+>>> 
+===================================================================== RESTART: C:/Users/Public/python/.git/file.py =====================================================================
+Gec Vaishali
+>>> 
+===================================================================== RESTART: C:/Users/Public/python/.git/file.py =====================================================================
+Hello
