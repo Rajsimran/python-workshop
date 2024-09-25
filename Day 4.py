b23401b9e378b29fb2522a20a20e62bae42b0645
Rajsimran <simranraj725060@gmail.com>
2024-09-25 13:08:02 +0530

    Add files via upload

---
+++ Day 4.py
@@ -0,0 +1,209 @@
+Python 3.12.6 (tags/v3.12.6:a4a2d2b, Sep  6 2024, 20:11:23) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print("Hello world")
+Hello world
+3+4
+7
+3**7
+2187
+2**4
+16
+"MCE"*3
+'MCEMCEMCE'
+"NCE"*3
+'NCENCENCE'
+'NEC'*3
+'NECNECNEC'
+x=6
+type(6)
+<class 'int'>
+x="SIMRAN"
+type(X)
+Traceback (most recent call last):
+  File "<pyshell#10>", line 1, in <module>
+    type(X)
+NameError: name 'X' is not defined. Did you mean: 'x'?
+type(6)
+<class 'int'>
+x="SIMRAM"
+x="Simran"
+type(X)
+Traceback (most recent call last):
+  File "<pyshell#14>", line 1, in <module>
+    type(X)
+NameError: name 'X' is not defined. Did you mean: 'x'?
+>>> type(x)
+<class 'str'>
+>>> x=6
+>>> name="Simran"
+>>> first_name="Simran"
+>>> 
+>>> 
+>>> first_name
+'Simran'
+>>> print("Hello world")
+Hello world
+>>> x=7
+>>> print(x)
+7
+>>> x=input("Enter a number:")
+Enter a number:6
+>>> print(x)
+6
+>>> x,y=input("Enter two number:")
+Enter two number:3
+Traceback (most recent call last):
+  File "<pyshell#27>", line 1, in <module>
+    x,y=input("Enter two number:")
+ValueError: not enough values to unpack (expected 2, got 1)
+>>> x,y=input("Enter two number:")
+Enter two number:34
+>>> Enter CGPA:8.7
+SyntaxError: invalid syntax
+>>> Enter CGPA:8.7
+SyntaxError: invalid syntax
+>>> Enter CGPA:6.7
+SyntaxError: invalid syntax
+>>> x=int(input("Enter a number:"))
+Enter a number:7
+>>> type(7)
+<class 'int'>
+>>> y=float(input("Enter CGPA:"))
+Enter CGPA:8.7
+>>> type(y)
+<class 'float'>
+>>> x,y=input("Enter two numbers:").split()
+Enter two numbers:8 7
+>>> x=int(X)
+Traceback (most recent call last):
+  File "<pyshell#37>", line 1, in <module>
+    x=int(X)
+NameError: name 'X' is not defined. Did you mean: 'x'?
+x=
+SyntaxError: invalid syntax
+x=int(x)
+y=int(y)
+x+y
+15
+x,y=input("Enter two numbers:").split()
+Enter two numbers:87
+Traceback (most recent call last):
+  File "<pyshell#42>", line 1, in <module>
+    x,y=input("Enter two numbers:").split()
+ValueError: not enough values to unpack (expected 2, got 1)
+Enter two numbers:3
+SyntaxError: invalid syntax
+4Enter two numbers:3 4
+SyntaxError: invalid decimal literal
+Enter two numbers:3 4
+SyntaxError: invalid syntax
+Enter two numbers : 6 7
+SyntaxError: invalid syntax
+Enter two numbers: 6 7
+SyntaxError: invalid syntax
+Enter two numbers:8 7
+SyntaxError: invalid syntax
+x,y=input("Enter two numbers:").split()
+Enter two numbers:6 7
+x+y
+'67'
+print ("Hello \n world")
+Hello 
+ world
+print("Hello\nworld")
+Hello
+world
+7**5
+16807
+7**2
+49
+4//2
+2
+4.5//2
+2.0
+5//2
+2
+x=7
+x**=2
+x
+49
+x=["Simran","Loctus"]
+print('Is value Present?',"Simran" in x)
+Is value Present? True
+x=["Simran" ,"Lotus"]
+print("Simran" in x)
+True
+print("{Simran" in x)
+False
+x=["Simran","Lotus"]
+print("Simran" in x)
+True
+print("Loctus"not in x)
+True
+print("Lotus" not in x)
+False
+a=["Rose",Lotus"]
+   
+SyntaxError: unterminated string literal (detected at line 1)
+a=["Rose","Lotus"]
+   
+b=["Rose","Lotus"]
+   
+c = a
+   
+print(a is c)
+   
+True
+x=6
+   
+y=7
+   
+print(x is y)
+   
+False
+y=x
+   
+print(x is y)
+   
+True
+x="Simran"
+   
+y=2
+   
+print(x is y)
+   
+False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
